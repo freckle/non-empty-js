9807f3fed25858f1577960a929bdef187d090748
✿ corey (he/they) <corey@x64.co>
2022-06-07 11:30:11 -0700

    Migrate and generate interfaces

---
+++ dist/index.d.ts
@@ -0,0 +1,30 @@
+import { type ArrayIterator, type ListIterator } from 'lodash';
+declare class NonEmpty {
+}
+export declare type NonEmptyArray<T> = Array<T> & NonEmpty;
+export declare function mkNonEmpty<T>(array: Array<T>): NonEmptyArray<T> | undefined | null;
+export declare function mkNonEmptyFromJust<T>(array: Array<T>): NonEmptyArray<T>;
+export declare function mkNonEmptySingleton<T>(elem: T): NonEmptyArray<T>;
+export declare function mkNonEmptyFromHead<T>(head: T, tail: Array<T>): NonEmptyArray<T>;
+export declare function mkNonEmptyFromLast<T>(init: Array<T>, last: T): NonEmptyArray<T>;
+export declare function mapOnNonEmpty<T, U>(nonEmpty: NonEmptyArray<T>, f: ListIterator<T, U> | ArrayIterator<T, U>): NonEmptyArray<U>;
+export declare function lastOnNonEmpty<T>(array: NonEmptyArray<T>): T;
+export declare function headOnNonEmpty<T>(array: NonEmptyArray<T>): T;
+export declare function tailOnNonEmpty<T>(array: NonEmptyArray<T>): Array<T>;
+export declare function initOnNonEmpty<T>(array: NonEmptyArray<T>): Array<T>;
+export declare function nonEmptyToArray<T>(array: NonEmptyArray<T>): Array<T>;
+export declare function unconsOnNonEmpty<T>(array: NonEmptyArray<T>): [T, Array<T>];
+export declare function flattenOnNonEmpty<T>(array: NonEmptyArray<NonEmptyArray<T>>): NonEmptyArray<T>;
+declare const _default: {
+    mkNonEmpty: typeof mkNonEmpty;
+    mkNonEmptySingleton: typeof mkNonEmptySingleton;
+    mkNonEmptyFromHead: typeof mkNonEmptyFromHead;
+    mkNonEmptyFromLast: typeof mkNonEmptyFromLast;
+    lastOnNonEmpty: typeof lastOnNonEmpty;
+    headOnNonEmpty: typeof headOnNonEmpty;
+    tailOnNonEmpty: typeof tailOnNonEmpty;
+    unconsOnNonEmpty: typeof unconsOnNonEmpty;
+    initOnNonEmpty: typeof initOnNonEmpty;
+    nonEmptyToArray: typeof nonEmptyToArray;
+};
+export default _default;
