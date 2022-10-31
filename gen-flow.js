813feaa89f11b1f5f03d538c001307b29c7b4b6b
✿ corey (he/they) <corey@x64.co>
2022-10-31 11:36:56 -0700

    Fix generated Flow interfaces
    Add minimal `.flowconfig` and lodash interface to validate that the generated Flow interface is correct. This requires some string replacements of type names used from the lodash TypeScript library to their Flow equivalents.

--- gen-flow.js
+++ gen-flow.js
@@ -7,15 +7,28 @@ glob('./dist/**/*.d.ts', {}, (err, files) => {
   if (err) console.error(err)
   files.forEach(f => {
     const flowdef = beautify(compiler.compileDefinitionFile(f))
+    const fixedFlowdef = fixFlowdef(flowdef)
     const p = path.parse(f)
     const name = /(.*).d/.exec(p.name)[1]
     fs.writeFile(
       `${p.dir}/${name}.js.flow`,
       `//@flow
-${flowdef}`,
+${fixedFlowdef}`,
       e => {
         if (e) console.error(e)
       }
     )
   })
 })
+
+const fixFlowdef = module => {
+  return (
+    module
+      // https://github.com/joarwilk/flowgen/issues/185
+      .replace(/\btype,\s+/g, 'type ')
+      // Replace lodash TypeScript types with Flow equivalents
+      .replaceAll('type ArrayIterator, type ListIterator', 'type ReadOnlyMapIterator') // import
+      .replaceAll('ArrayIterator', 'ReadOnlyMapIterator') // tokens
+      .replaceAll('ListIterator', 'ReadOnlyMapIterator')
+  )
+}
